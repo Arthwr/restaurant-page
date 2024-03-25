48af182b268acd3976830b9076c2caf40aa3c428
arthur <artyr007@gmail.com>
2024-03-25 20:11:45 +0500

    Add home link redirect from main logo

--- src/index.js
+++ src/index.js
@@ -4,20 +4,18 @@ import loadNavImages from "./nav.js";
 import handleLinks from "./handleNav.js";
 
 loadNavImages();
-const content = document.getElementById("content");
-const homeLink = document.querySelector(".main");
+const mainLinks = document.querySelectorAll(".main");
+
+mainLinks.forEach((link) => {
+  link.addEventListener("click", () => handleLinks(main));
+});
+
 const aboutLink = document.querySelector(".about");
 const menuLink = document.querySelector(".menu");
 const contactLink = document.querySelector(".contact");
 
-homeLink.addEventListener("click", () => handleLinks(main));
 aboutLink.addEventListener("click", () => handleLinks(about));
 menuLink.addEventListener("click", () => handleLinks(menu));
 contactLink.addEventListener("click", () => handleLinks(contact));
-
-// About.js
-// import aboutImg from "./images/about-chef.webp";
-// const chefImg = document.querySelector(".about-content > img");
-// chefImg.src = aboutImg;
 
 handleLinks(main);
